54b4f431fc934828c96a0aa281b77e75d8970da1
Chukwuemeka <mekastans@gmail.com>
2024-01-15 18:41:28 +0100

    Start Insert nosql

--- using-nosql-db/insert.py
+++ using-nosql-db/insert.py
@@ -3,11 +3,18 @@ from pymongo import MongoClient
 
 # Connect to MongoDB
 client = MongoClient('mongodb://localhost:27017/')
-db = client['dataengineering']
+db_name = 'dataengineering'
+db = client[db_name]
+
+# Check if the database exists, create it if not
+if db_name not in client.list_database_names():
+    print(f"Creating MongoDB database: {db_name}")
+    db = client[db_name]
+    
 collection = db['users']
 
 # Fake()r instance
-fake = Faker
+fake = Faker()
 
 # Loop 1000 times to generate and insert data
 for _ in range(1000):

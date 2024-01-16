785be912479adbd39625bd7413dd191c982a83a3
Chukwuemeka <mekastans@gmail.com>
2024-01-16 14:44:15 +0100

    EDA Start

---
+++ EDA/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df=pd.read_csv('scooter.csv')
+
+# View the available columns in the dataset
+print(df.columns)
+
+# View the column datatypes
+print(df.dtypes)
+
+print(df.head(5))
+
+# Setting the number of columns to show
+pd.set_option('display.max_columns', 500)
